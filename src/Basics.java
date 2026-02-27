d90d988d42082d13f731fb18c303d6f594edbce1
Mani <bethina.vm@gmail.com>
2026-02-27 12:00:35 +0200

    Added code for update address using PUT

--- src/Basics.java
+++ src/Basics.java
@@ -1,39 +1,48 @@
 import io.restassured.RestAssured;
-//import io.restassured.path.json.JsonPath;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
+import org.testng.Assert;
 
+import files.ReUsableMethods;
+import files.payload;
 
 public class Basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// validate if Add Place API is working as expected 
+// validate if Add Place API is workimg as expected 
 		//Add place-> Update Place with New Address -> Get Place to validate if New address is present in response
 		
 		//given - all input details 
 		//when - Submit the API -resource,http method
 		//Then - validate the response
 		RestAssured.baseURI= "https://rahulshettyacademy.com";
+		String response=given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(payload.AddPlace()).when().post("maps/api/place/add/json")
+		.then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+		
+		System.out.println(response);
+		JsonPath js=new JsonPath(response); //for parsing Json
+		String placeId=js.getString("place_id");
+		
+		System.out.println(placeId);
+		
+		//Update Place
+		String newAddress = "Summer Walk, Africa";
+		
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Ceder Roc\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"2928, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}\r\n"
-				+ "").when().post("maps/api/place/add/json\"").then().log().all().assertThat().statusCode(200);
+		.body("{\r\n" + 
+				"\"place_id\":\""+placeId+"\",\r\n" + 
+				"\"address\":\""+newAddress+"\",\r\n" + 
+				"\"key\":\"qaclick123\"\r\n" + 
+				"}").
+		when().put("maps/api/place/update/json")
+		//equalTO Hamcrest-matchers
+		.then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
 		
 	
 	
