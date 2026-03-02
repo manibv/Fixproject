436c99611c9842e00c776e40d3728c2aff065f9c
Mani <bethina.vm@gmail.com>
2026-03-02 13:35:06 +0200

    Added gitignore and removed target folder from tracking

--- src/Basics.java
+++ src/Basics.java
@@ -64,7 +64,7 @@ public class Basics {
 		JsonPath js1=ReUsableMethods.rawToJson(getPlaceResponse);
 		String actualAddress =js1.getString("address");
 		System.out.println(actualAddress);
-		Assert.assertEquals(actualAddress, "Pacific ocean");
+		Assert.assertEquals(actualAddress, "Pacific ocean"); //this address is in correct should be "Summer Walk, Africa".
 		
 	
 	
